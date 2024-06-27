08a8c7029d29c99bbec8d66479b86d570160e912
cesareal9 <caugustorama@gmail.com>
2024-06-27 20:00:22 -0300

    atualização exercicios

--- aula2706.js
+++ aula2706.js
@@ -14,22 +14,23 @@
 
 // 
 
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 
-let base = 0;
-let expoente = 0;
+// let base = 0;
+// let expoente = 0;
 
-base = Number(prompt("adicione um numero para a base: "));
-expoente = Number(prompt("adicione um numero para o expoente: "));
+// base = Number(prompt("adicione um numero para a base: "));
+// expoente = Number(prompt("adicione um numero para o expoente: "));
 
-let resultado = 1;
-for (let i = 0; i < expoente; i++);
-{
-  let temp = 0;
-  for (let j = 0; j < base; j++) {
-    temp += resultado;
-  }
-  resultado = temp;
-}
-console.log(`${base}^${expoente} = ${resultado}`);
+// let resultado = 1;
+// for (let i = 0; i < expoente; i++);
+// {
+//   let temp = 0;
+//   for (let j = 0; j < base; j++) {
+//     temp += resultado;
+//   }
+//   resultado = temp;
+// }
+// console.log(`${base}^${expoente} = ${resultado}`);
+
 // 
