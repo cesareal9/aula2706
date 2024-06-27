1e4dc164e6e9484c7f00b26a233e264b5493a6da
cesareal9 <caugustorama@gmail.com>
2024-06-27 19:50:34 -0300

    mensagem

---
+++ aula2706.js
@@ -0,0 +1,35 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+
+// const number = parseInt(prompt("Digite um numero "));
+
+// if (isNaN(number) || number < 0) {
+//   console.log("digite um numero inteiro não negativo ");
+// } else {
+//   let result = 1;
+//   for (let i = 2; i <= number; i++){
+//     result *= i;
+//   }
+//   console.log(`o fatorial de ${number} é ${result}.`);
+// }
+
+// 
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let base = 0;
+let expoente = 0;
+
+base = Number(prompt("adicione um numero para a base: "));
+expoente = Number(prompt("adicione um numero para o expoente: "));
+
+let resultado = 1;
+for (let i = 0; i < expoente; i++);
+{
+  let temp = 0;
+  for (let j = 0; j < base; j++) {
+    temp += resultado;
+  }
+  resultado = temp;
+}
+console.log(`${base}^${expoente} = ${resultado}`);
+// 
